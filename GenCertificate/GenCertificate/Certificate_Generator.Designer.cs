8c5f234bababd810afb8602e668750e476b864e2
ViKa_618 <hovikhanh2k4@gmail.com>
2024-05-19 21:49:38 +0700

    Add .gitattributes to enforce LF line endings for Markdown files

---
+++ GenCertificate/GenCertificate/Certificate_Generator.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace GenCertificate
+{
+    partial class Certificate_Generator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbIssuer = new System.Windows.Forms.TextBox();
+            this.tbSubject = new System.Windows.Forms.TextBox();
+            this.numericUpDownMonths = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbFolderName = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonths)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Issuer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Subject";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(73, 147);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valid for";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(73, 212);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Output Folder";
+            // 
+            // tbIssuer
+            // 
+            this.tbIssuer.Location = new System.Drawing.Point(217, 23);
+            this.tbIssuer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbIssuer.Name = "tbIssuer";
+            this.tbIssuer.Size = new System.Drawing.Size(362, 27);
+            this.tbIssuer.TabIndex = 4;
+            this.tbIssuer.Text = "CN=Google";
+            // 
+            // tbSubject
+            // 
+            this.tbSubject.Location = new System.Drawing.Point(217, 83);
+            this.tbSubject.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbSubject.Name = "tbSubject";
+            this.tbSubject.Size = new System.Drawing.Size(623, 27);
+            this.tbSubject.TabIndex = 5;
+            this.tbSubject.Text = "C=GB, ST=Berkshire, L=Reading, O=Bill";
+            // 
+            // numericUpDownMonths
+            // 
+            this.numericUpDownMonths.Location = new System.Drawing.Point(217, 147);
+            this.numericUpDownMonths.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.numericUpDownMonths.Name = "numericUpDownMonths";
+            this.numericUpDownMonths.Size = new System.Drawing.Size(86, 27);
+            this.numericUpDownMonths.TabIndex = 6;
+            this.numericUpDownMonths.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(309, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "months";
+            // 
+            // tbFolderName
+            // 
+            this.tbFolderName.Location = new System.Drawing.Point(217, 209);
+            this.tbFolderName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbFolderName.Name = "tbFolderName";
+            this.tbFolderName.Size = new System.Drawing.Size(440, 27);
+            this.tbFolderName.TabIndex = 8;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(686, 209);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(154, 27);
+            this.btnBrowse.TabIndex = 9;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(636, 278);
+            this.btnGenerate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(84, 32);
+            this.btnGenerate.TabIndex = 10;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(747, 278);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(84, 32);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Certificate_Generator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 328);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbFolderName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDownMonths);
+            this.Controls.Add(this.tbSubject);
+            this.Controls.Add(this.tbIssuer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Name = "Certificate_Generator";
+            this.Text = "X509 Certificate Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonths)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbIssuer;
+        private System.Windows.Forms.TextBox tbSubject;
+        private System.Windows.Forms.NumericUpDown numericUpDownMonths;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbFolderName;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ GenCertificate/GenCertificate/Certificate_Generator.cs
@@ -0,0 +1,81 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.OpenSsl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenCertificate
+{
+    public partial class Certificate_Generator : Form
+    {
+        public Certificate_Generator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FolderBrowser = new FolderBrowserDialog();
+            FolderBrowser.Description = "Select the folder to store the certificates";
+            DialogResult result = FolderBrowser.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                tbFolderName.Text = FolderBrowser.SelectedPath;
+                btnGenerate.Enabled = tbFolderName.Text.Length > 0;
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                AsymmetricCipherKeyPair CertificateKey;
+
+                //let us first generate the root certificate
+                X509Certificate2 X509RootCert = Cryptography.CreateCertificate(tbSubject.Text, tbIssuer.Text, (int)numericUpDownMonths.Value, out CertificateKey);
+
+                //now let us write the certificates files to the folder 
+                File.WriteAllBytes(tbFolderName.Text + "\\" + "X509Cert.der", X509RootCert.RawData);
+
+                string PublicPEMFile = tbFolderName.Text + "\\" + "X509Cert-public.pem";
+                string PrivatePEMFile = tbFolderName.Text + "\\" + "X509Cert-private.pem";
+
+                //now let us also create the PEM file as well in case we need it
+                using (TextWriter textWriter = new StreamWriter(PublicPEMFile, false))
+                {
+                    PemWriter pemWriter = new PemWriter(textWriter);
+                    pemWriter.WriteObject(CertificateKey.Public);
+                    pemWriter.Writer.Flush();
+                }
+
+                //now let us also create the PEM file as well in case we need it
+                using (TextWriter textWriter = new StreamWriter(PrivatePEMFile, false))
+                {
+                    PemWriter pemWriter = new PemWriter(textWriter);
+                    pemWriter.WriteObject(CertificateKey.Private);
+                    pemWriter.Writer.Flush();
+                }
+
+                MessageBox.Show("The security certificates have been succcessfully generated.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while generating certificates. " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+ }
+
